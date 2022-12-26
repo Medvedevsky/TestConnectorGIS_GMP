b00b4f2a4ff5df9ae7d1b333ab594264d20672d0
Medvedevsky <killumexxx@gmail.com>
2022-12-26 19:24:37 +0300

    feat: Add CheckPay model

---
+++ ConnectorGIS_GMP/ConnectorGIS_GMP/ApiClient/Model/CheckPay.cs
@@ -0,0 +1,72 @@
+﻿using ConnectorGIS_GMP.ApiClient.Enum;
+
+namespace ConnectorGIS_GMP.ApiClient.Model
+{
+    /// <summary>
+    /// Модель передаваемая в метод CheckPay.
+    /// Больше инфы в API - https://shtraf.biz/API_shtraf.biz_Manual.pdf
+    /// </summary>
+    public class CheckPay
+    {
+        public int Top { get; set; } = 1;
+        public string Id { get; set; } = string.Empty;
+        public string Hash { get; set; } = string.Empty;
+        public TypeSearch Type { get; set; }
+
+        /// <summary>
+        /// Номер водительского удостоверения.
+        /// Необязательное поле если в запросе есть sts.
+        /// Поле передается только при type==10.
+        /// </summary>
+        public string? Vu { get; set; }
+
+        /// <summary>
+        /// Номер Свидетельства о регистрации транспортного средства.
+        /// Необязательное поле, если в запросе присутствует поле vu.
+        /// Обязательное поле, если в запросе присутствует поле num.
+        /// </summary>
+        public string? Sts { get; set; }
+        /// <summary>
+        /// Реквииты организации ИП для поиска штрафов ГИБДД.(Смотреть АПИ).
+        /// </summary>
+        public string? Org { get; set; }
+
+        /// <summary>
+        /// Номер Постановления об административном правонарушении.
+        /// Передается вместо полей vu, sts, num, pasp.
+        /// </summary>
+        public string? Num { get; set; }
+
+        /// <summary>
+        /// Необязательное поле, если в запросе присутствует поле snilsn.
+        /// Поле передается только при type==21 или type==24.
+        /// Можно передать сразу несколько через ; (до 10).
+        /// </summary>
+        public string? Inn { get; set; }
+
+        /// <summary>
+        /// Необязательное поле,если в запросе присутствует поле inn.
+        /// Поле передается толькопри type==21 или type==24.
+        /// </summary>
+        public string? Snils { get; set; }
+
+        /// <summary>
+        ///  Серия и номер паспорта
+        ///  Необязательное поле,если в запросе присутствует поле inn или snils или vu или sts.
+        /// Поле передается только при type==10 и type==24 .
+        /// </summary>
+        public string? Pasp { get; set; }
+
+        /// <summary>
+        /// УИН при type==21 или type==24.
+        /// Передается вместо полей inn, snils, pasp.
+        /// </summary>
+        public string? Ind { get; set; }
+
+        /// <summary>
+        /// 0 - искать только неоплаченные  начисления.
+        /// 1 - искать оплаченные и неоплаченные начисления.
+        /// </summary>
+        public int Paid { get; set; } = 0;
+    }
+}
