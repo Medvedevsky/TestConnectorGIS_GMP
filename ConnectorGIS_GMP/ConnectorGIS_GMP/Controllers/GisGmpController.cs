762cff6abd45ecf9adb2c146d652d806508cdca5
Medvedevsky <killumexxx@gmail.com>
2022-12-27 17:20:45 +0300

    feat: GisGmpController

---
+++ ConnectorGIS_GMP/ConnectorGIS_GMP/Controllers/GisGmpController.cs
@@ -0,0 +1,25 @@
+﻿using ConnectorGIS_GMP.ApiClient;
+using ConnectorGIS_GMP.ApiClient.Model.Request;
+using ConnectorGIS_GMP.ApiClient.Model.Response;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ConnectorGIS_GMP.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+    public class GisGmpController : ControllerBase
+    {
+        private readonly GisGmpClient _gisGmpClient;
+        public GisGmpController(GisGmpClient gisGmpClient)
+        {
+            _gisGmpClient = gisGmpClient;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<CheckPayResponse?>> SearchCalculation(CheckPayRequest request)
+        {
+            return await _gisGmpClient.Search(request);
+        }
+    }
+}
